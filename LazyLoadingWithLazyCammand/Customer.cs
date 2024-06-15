5a6f2053f89d5ee5f489a4d8f54997d7223cacb5
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-06-15 08:32:58 +0330

    Add project files.

---
+++ LazyLoadingWithLazyCammand/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoadingWithLazyCammand
+{
+    internal class Customer
+    {
+        private readonly Lazy<IEnumerable<Order>> _orders;
+
+        public Customer()
+        {
+            _orders = new Lazy<IEnumerable<Order>>(LoadOrders);
+        }
+
+        private IEnumerable<Order> LoadOrders()
+        {
+            List<Order> orders = new List<Order>();
+            Parallel.ForEach(Enumerable.Range(0, 5), x => {
+                orders.Add(new Order
+                {
+                    Id = x
+                });
+            });
+
+            return orders;
+        }
+
+        public bool IsValueCreated => _orders.IsValueCreated;
+        public IEnumerable<Order> GetOrders => _orders.Value;   
+    }
+
+    internal class Order
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ LazyLoadingWithLazyCammand/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LazyLoadingWithLazyCammand;
+
+Customer customer = new();
+
+Console.WriteLine(customer.IsValueCreated);
+
+foreach (var item in customer.GetOrders)
+{
+    Console.WriteLine(item.Id);
+}
+
+Console.WriteLine(customer.IsValueCreated);
